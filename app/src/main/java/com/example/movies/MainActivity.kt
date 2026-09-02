d11ea1907cee659c3c9b2995254ea829c77534dd
simi-koder <simon.corej@gmail.com>
2026-09-02 15:02:54 +0200

    Added db sharing feature

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -1,72 +1,84 @@
 package com.example.movies
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.activity.enableEdgeToEdge
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Button
-import android.widget.TextView
-import androidx.navigation.NavController
-import com.example.movies.databinding.ActivityMainBinding
-import com.example.movies.databinding.StartPageBinding
+    import android.annotation.SuppressLint
+    import android.content.Intent
+    import android.os.Bundle
+    import androidx.appcompat.app.AppCompatActivity
+    import androidx.activity.enableEdgeToEdge
+    import androidx.core.view.ViewCompat
+    import androidx.core.view.WindowInsetsCompat
+    import androidx.navigation.findNavController
+    import androidx.navigation.fragment.NavHostFragment
+    import androidx.navigation.ui.AppBarConfiguration
+    import androidx.navigation.ui.navigateUp
+    import androidx.navigation.ui.setupActionBarWithNavController
+    import android.view.Menu
+    import android.view.MenuItem
+    import androidx.core.content.FileProvider
+    import androidx.navigation.NavController
+    import com.example.movies.databinding.ActivityMainBinding
 
 
-@SuppressLint("WrongViewCast")
-class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
+    class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+        private lateinit var appBarConfiguration: AppBarConfiguration
+        private lateinit var binding: ActivityMainBinding
 
-    private lateinit var navController: NavController
+        private lateinit var navController: NavController
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            enableEdgeToEdge()
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+            setSupportActionBar(binding.toolbar)
+
+            val navHostFragment = supportFragmentManager
+                .findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+            navController = navHostFragment.navController
+
+            val prefs = getSharedPreferences("app_prefs", MODE_PRIVATE)
+            val hasSeenOnboarding = prefs.getBoolean("has_seen_onboarding", false)
+
+            val navGraph = navController.navInflater.inflate(R.navigation.nav_graph)
+
+            navGraph.setStartDestination(
+                if (hasSeenOnboarding) R.id.MovieList else R.id.StartPage
+            )
+
+            navController.graph = navGraph
+
+            appBarConfiguration = AppBarConfiguration(
+                setOf(R.id.MovieList, R.id.StartPage)
+            )
+            setupActionBarWithNavController(navController, appBarConfiguration)
         }
-        setSupportActionBar(binding.toolbar)
 
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
-        navController = navHostFragment.navController
+        private fun exportDatabase() {
+            val dbFile = getDatabasePath("new2_database.db")
+            val dbUri = FileProvider.getUriForFile(
+                this,
+                "$packageName.provider",
+                dbFile
+            )
 
-        val prefs = getSharedPreferences("app_prefs", MODE_PRIVATE)
-        val hasSeenOnboarding = prefs.getBoolean("has_seen_onboarding", false)
-
-        val navGraph = navController.navInflater.inflate(R.navigation.nav_graph)
-
-        navGraph.setStartDestination(
-            if (hasSeenOnboarding) R.id.MovieList else R.id.StartPage
-        )
-
-        navController.graph = navGraph
-
-        appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.MovieList, R.id.StartPage)
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-    }
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "application/octet-stream"
+                putExtra(Intent.EXTRA_STREAM, dbUri)
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            }
+            startActivity(Intent.createChooser(shareIntent, "Zdieľať databázu"))
+        }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
@@ -81,6 +93,11 @@ class MainActivity : AppCompatActivity() {
 
             R.id.action_user -> {
                 navController.navigate(R.id.AddUser)
+                true
+            }
+
+            R.id.action_send_db -> {
+                exportDatabase()
                 true
             }
 
