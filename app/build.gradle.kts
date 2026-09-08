ad52dd7d4aef45c231479c31b726b7f6c81fa5dc
simi-koder <simon.corej@gmail.com>
2026-09-08 11:40:53 +0200

    updated version, changed text color

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 37
         versionCode = 2
-        versionName = "2.5"
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
